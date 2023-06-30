Bug fixes, now functioning updates for Hermitian matrices. Non-hermitian algorithm works as well for hermitian matrix, but seems to run into precision issues

krypy/mfuncs.py
@@ -1,4 +1,5 @@
-from .utils import Arnoldi, ArgumentError
+from .utils import (Arnoldi, ArgumentError,
+                    arnoldi_res)
 import numpy
 import scipy.linalg
 import scipy.sparse
@@ -21,7 +22,7 @@ class MatrixFunction:
             f,
             f_description=None,
             implementation="scipy",
-            dropping_tol=1e-14  # How to implement? 
+            dropping_tol=1e-14  # How to implement?
     ):
         """DOCSTRING?"""
         self.f = numpy.vectorize(f)
@@ -102,8 +103,7 @@ class MatrixExponential(MatrixFunction):
         return scipy.sparse.linalg.expm(A_sp)
 
     def _evaluate_hermitian_sparse(self, A_sp, is_pos_semidefinite=False):
-        raise NotImplementedError("Unknown sparse implementation:"
-                                  + " {}".format(self.implementation))
+        return self._evaluate_general_sparse(A_sp)
 
 
 class MatrixInverse(MatrixFunction):
@@ -125,7 +125,7 @@ class MatrixInverse(MatrixFunction):
         """From scipy notes*** NEED TO CITE"""
         if self.implementation == "scipy":
             return scipy.linalg.inv(A)
-        elif self.implementation == "diagonalizable":
+        elif self.implementation == "hermitian":
             super()._evaluate_hermitian(A, is_pos_semidefinite)
         else:
             raise NotImplementedError("Unknown evaluation algorithm:"
@@ -135,8 +135,7 @@ class MatrixInverse(MatrixFunction):
         return scipy.sparse.linalg.inv(A_sp)
 
     def _evaluate_hermitian_sparse(self, A_sp, is_pos_semidefinite=False):
-        raise NotImplementedError("Unknown evaluation algorithm:"
-                                  + " {}".format(self.implementation))
+        return self._evaluate_general_sparse(A_sp)
 
 
 class MatrixFunctionSystem:
@@ -174,18 +173,18 @@ class MatrixFunctionSystem:
         return self.fA
 
     def __repr__(self):
-        s = "MatrixFunctionSystem object\n"
-        num_nonzero = (self.A.nnz if self.is_sparse 
-                       else sum(sum(self.A == numpy.zeros(self.A.shape))))
+        s = str(type(self)) + " object\n"
+        num_nonzero = (self.A.nnz if self.is_sparse
+                       else sum(sum(self.A != numpy.zeros(self.A.shape))))
         s += "    A: has dimensions {} and {} non-zero elements\n".format(
             self.A.shape,
             num_nonzero
             )
+        s += "    type(A): {}\n".format(type(self.A))
         s += "    f: {}\n".format(self.mfunc.f_description)
-        s += ("    f(A) storage: " + self.diag_only * "diagonal elements\n"
-              + (not self.diag_only) * "all elements\n")
+        s += "    f(A) diagonal storage: {}\n".format(str(self.diag_only))
         s += "    Normal: " + str(self.normal) + "\n"
-        s += "    Hermitian:" + str(self.hermitian) + "\n"
+        s += "    Hermitian: " + str(self.hermitian) + "\n"
         s += "    Positive-semidefinite: " + str(self.pos_semidefinite)
         return s
 
@@ -201,11 +200,11 @@ class RankOneUpdate:
                  rel_tol=1e-5,
                  d=2,
                  ortho="dmgs",
-                 explicit_soln=None,
+                 explicit_update=None,
                  dropping_tol=None,
                  store_res=True):
         #  Clean params, initialize system
-        self.N = N = b.shape[0]
+        self.N = b.shape[0]
         if not isinstance(mfunc_system, MatrixFunctionSystem):
             raise TypeError("Unrecognized type {}. ".format(type(mfunc_system))
                             + "Should be a MatrixFunctionSystem.")
@@ -215,29 +214,36 @@ class RankOneUpdate:
         self.mfunc = mfunc_system.get_mfunc()
         self.diag_only = mfunc_system.diag_only
         self.b = b
-        self.c = c if c else b
+        c_given = False if c is None or c.size == 0 else True
+        self.c = c if c_given else b
         self.sign = sign
-        self.maxiter = maxiter if maxiter else N - 1
+        self.maxiter = maxiter if maxiter else self.N - 1
         self.rel_tol = rel_tol
         self.d = d
-        self.explicit_soln = explicit_soln
+        self.explicit_update = explicit_update
         self.dropping_tol = dropping_tol
         self.ortho = ortho
 
         #  Construct important properties
-        self.hermitian = min([numpy.all(b == c), mfunc_system.hermitian])
+        self.hermitian = min([numpy.all(self.b == self.c),
+                              mfunc_system.hermitian])
         self.l_arnoldi = Arnoldi(self.A,
-                                       self.b,
-                                       self.maxiter,
-                                       self.ortho)
+                                 self.b,
+                                 self.maxiter,
+                                 self.ortho)
         if not self.hermitian:
             self.r_arnoldi = Arnoldi(self.A.conj().T,
-                                           self.sign * self.c,
-                                           self.maxiter,
-                                           self.ortho)
+                                     self.sign * self.c,
+                                     self.maxiter,
+                                     self.ortho)
         else:
             self.r_arnoldi = self.l_arnoldi
-        self.res = numpy.zeros(N) if store_res else None
+
+        if store_res:
+            self.res = numpy.zeros(self.N)
+            self.res[0] = float('inf')
+        else:
+            self.res = None
         self.Xkf = None
         self.oldXkfs = deque()
 
@@ -248,7 +254,7 @@ class RankOneUpdate:
         if k > 1 and len(self.oldXkfs) < self.d:
             self.oldXkfs.append(self.Xkf)
         elif k > 1 and len(self.oldXkfs) >= self.d:
-            self.oldXkfs.popleft(0)
+            self.oldXkfs.popleft()
             self.oldXkfs.append(self.Xkf)
 
         if self.hermitian:
@@ -257,7 +263,8 @@ class RankOneUpdate:
             self.r_arnoldi.advance()
             self.Xkf = self._compute_Xkf_nonhermitian()
 
-        self.res[k-1] = self._estimate_residual()
+        if self.res is not None:
+            self.res[k-1] = self._estimate_residual()
 
     def _compute_Xkf_hermitian(self):
         iter = self.get_iter()
@@ -278,22 +285,27 @@ class RankOneUpdate:
 
         size_b = numpy.linalg.norm(self.b, 2)
         size_c = numpy.linalg.norm(self.c, 2)
-        e1 = numpy.zeros((self.N, 1))
+        e1 = numpy.zeros((k, 1))
         e1[0, 0] = 1
         M1 = G[:k, :k]
         M2 = (size_b * size_c * e1) @ e1.conj().T
         M3 = numpy.zeros((k, k))
-        M4 = H[:k, :k] + (size_c * (V.conj().T @ self.b)) @ e1.conj().T
-
-        F = self.mfunc(numpy.block(
+        M4 = H[:k, :k] + (size_c * (V[:, :k].conj().T @ self.b)) @ e1.conj().T
+        T = numpy.block(
             [[M1, M2],
              [M3, M4]]
-        ))
+        )
+        F = self.mfunc(T)
+        print(scipy.linalg.norm(M1 @ M1.T - M1.T @ M1), scipy.linalg.norm(M4 @ M4.T - M4.T @ M4))
+        print(scipy.linalg.norm(T @ T.T - T.T @ T))
         Xkf = F[:k, k:2*k]
         return Xkf
 
     def _estimate_residual(self):
-        if not self.Xkf:
+        if self.explicit_update is not None:
+            return self._compute_exact_residual()
+
+        if self.Xkf is None:
             return float("inf")
         elif len(self.oldXkfs) < self.d:
             return numpy.linalg.norm(self.Xkf)
@@ -304,27 +316,31 @@ class RankOneUpdate:
             R = self.Xkf
             R = R - numpy.block([[self.oldXkfs[0],     numpy.zeros((k, d))],
                                  [numpy.zeros((d, k)), numpy.zeros((d, d))]])
-            return numpy.linalg.norm(R, 2)
+            return numpy.linalg.norm(R, 2)  
+
+    def _compute_exact_residual(self):
+        return numpy.linalg.norm(self.get_update() - self.explicit_update, 2)    
 
     def _diagonal_update(self):
         raise NotImplementedError("No diagonal update defined yet.")
 
     def _full_update(self):
         U, G, V, H = self.get_arnoldi()
-        return U @ self.Xkf @ V.conj().T
-
-
+        if self.Xkf is None:
+            dim = (self.N, self.N)
+            return (numpy.zeros(dim) if not self.mfunc_system.is_sparse
+                    else scipy.sparse.csc_array(dim))
+        return U[:, :-1] @ self.Xkf @ V[:, :-1].conj().T
 
     def get_update(self):
-        if not self.Xkf:
+        if self.get_iter() < 1:
             raise ArgumentError("No iterations for"
-                                      + " low-rank update performed yet")
-        
+                                + " low-rank update performed yet")
+
         if self.diag_only:
             return self._diagonal_update()
         else:
             return self._full_update()
-
 
     def get_updated_matrix(self):
         return self.A + (self.sign * self.b) @ self.c.conj().T
@@ -349,3 +365,41 @@ class RankOneUpdate:
             U, G = self.l_arnoldi.get()
             V, H = self.r_arnoldi.get()
         return U, G, V, H
+
+    def __repr__(self):
+        s = "RankOneUpdate object\n"
+        s += "\n".join(str(self.mfunc_system).split("\n")[1:-3]) + "\n"
+        s += "    b: vector with {} elements and size {}\n".format(
+            self.N, scipy.linalg.norm(self.b))
+        s += "    c: vector with {} elements and size {}\n".format(
+            self.N, scipy.linalg.norm(self.c))
+        s += "    b == c: {}\n".format(str(numpy.all(self.b == self.c)))
+        s += "    type of update: {}, {}\n".format(
+            "additive" if self.sign == 1 else "subtractive",
+            "hermitian" if self.hermitian else "nonhermitian"
+        )
+        s += "    current iteration: {}\n".format(self.get_iter())
+        s += "    maximum iteration: {}\n".format(self.maxiter)
+        s = (s + "    error estimate: {}\n".format(self.res[self.get_iter()])
+             if self.res is not None else s)
+        s += "    target relative tolerance: {}\n".format(self.rel_tol)
+        if self.dropping_tol:
+            s += "    dropping tolerance: {}\n".format(self.dropping_tol)
+        U, G, V, H = self.get_arnoldi()
+        if self.hermitian and G.size > 0:
+            s += "    arnoldi residual: {}\n".format(arnoldi_res(self.A, U, G))
+            s += "    arnoldi condition number: {}\n".format(
+                numpy.linalg.cond(U) if U.size <= 1e8 else "N/A, size > 1e8"
+            )
+        elif G.size > 0:
+            s += "    left arnoldi residual: {}\n".format(arnoldi_res(self.A, U, G))
+            s += "    left arnoldi condition number: {}\n".format(
+                numpy.linalg.cond(U) if U.size <= 1e8 else "N/A, size > 1e8"
+            )
+            s += "    right arnoldi residual: {}\n".format(arnoldi_res(self.A.conj().T, V, H))
+            s += "    right arnoldi condition number: {}\n".format(
+                numpy.linalg.cond(V) if V.size <= 1e8 else "N/A, size > 1e8"
+            )
+        else:
+            s += "    arnoldi: unavailable\n"
+        return s